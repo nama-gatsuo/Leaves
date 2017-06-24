c80f747fd680399a80b4e8f04127075301c38f11
Ayumu Nagamatsu <ayumu.nagamatsu@gmail.com>
2017-06-24 16:26:56 +0200

    Add sound .

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -2,7 +2,41 @@ import $ from 'jquery'
 
 const MAX_COUNT = 10;
 let count = 0;
+let sound;
 
+// sound
+// http://phiary.me/webaudio-api-getting-started/
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+let context = new AudioContext();
+
+getAudioBuffer('/sound/notification.mp3');
+
+function getAudioBuffer(url, fn){
+    let req = new XMLHttpRequest();
+    req.responseType = 'arraybuffer';
+
+    req.onreadystatechange = () => {
+        if (req.readyState === 4) {
+            if (req.status === 0 || req.status === 200) {
+                context.decodeAudioData(req.response, buffer => {
+                    sound = buffer;
+                });
+            }
+        }
+    };
+
+    req.open('GET', url, true);
+    req.send('');
+}
+
+function playSound(buffer){
+    let source = context.createBufferSource();
+    source.buffer = buffer;
+    source.connect(context.destination);
+    source.start(0);
+}
+
+// socket
 let socket = io();
 
 socket.on('new', msg => {
@@ -27,6 +61,23 @@ socket.on('new', msg => {
 
     $item.append($dt, $dc, $dd, $dr);
     $list.prepend($item);
+
+    $item.css({
+        backgroundColor: '#000'
+    });
+    $item.velocity({
+        backgroundColor: '#fff'
+    }, 3000);
+
+    $item.click(()=>{
+        playSound(sound);
+        console.log('play');
+    });
+
+    if (sound) {
+        playSound(sound);
+        console.log('play');
+    }
 
     if (count > MAX_COUNT) {
         let $items = $list.children();
